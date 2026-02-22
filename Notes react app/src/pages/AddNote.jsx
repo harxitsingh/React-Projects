167617a7b15df54b8732194183f96ff163992502
Harshit Singh <harxitsingh@gmail.com>
2026-02-22 20:14:29 +0530

    Add files via upload

--- Notes react app/src/pages/AddNote.jsx
+++ Notes react app/src/pages/AddNote.jsx
@@ -1,5 +1,42 @@
-function AddNote() {
-  return <h2>Add Note Page</h2>;
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function AddNote({ notes, setNotes }) {
+
+  const [title, setTitle] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newNote = {
+      id: Date.now(),
+      title
+    };
+
+    setNotes([...notes, newNote]);
+
+    navigate("/notes");
+  };
+
+  return (
+    <div>
+      <h2>Add Note</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <br /><br />
+
+        <button type="submit">Add Note</button>
+      </form>
+    </div>
+  );
 }
 
 export default AddNote;
