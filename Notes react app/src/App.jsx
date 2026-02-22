b25df45cabf8ca189cd6a69381014a72ba8ed7b8
Harshit Singh <harxitsingh@gmail.com>
2026-02-22 20:12:58 +0530

    Add files via upload

--- Notes react app/src/App.jsx
+++ Notes react app/src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
 
 import Home from "./pages/Home";
 import Notes from "./pages/Notes";
@@ -6,31 +8,38 @@ import AddNote from "./pages/AddNote";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
-
+import NoteDetail from "./pages/NoteDetail";
 
 function App() {
 
+  const [notes, setNotes] = useState([
+    { id: 1, title: "Learn React" },
+    { id: 2, title: "Learn React Router" },
+    { id: 3, title: "Build Notes App" }
+  ]);
+
   return (
-   <BrowserRouter>
-   <Navbar/>
-   <Routes>
+    <BrowserRouter>
+    <div className="container">
+      <Navbar />
+      <Routes>
 
         <Route path="/" element={<Home />} />
 
-        <Route path="/notes" element={<Notes />} />
+  <Route path="/notes" element={<Notes notes={notes} setNotes={setNotes} />} />
 
-        <Route path="/add" element={<AddNote />} />
+        <Route path="/notes/:id" element={<NoteDetail notes={notes} />} />
+
+        <Route path="/addnote" element={<AddNote notes={notes} setNotes={setNotes} />} />
 
         <Route path="/about" element={<About />} />
 
         <Route path="*" element={<NotFound />} />
 
-
-
-   </Routes>   
-   </BrowserRouter>
-    
-  )
+      </Routes>
+       </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
